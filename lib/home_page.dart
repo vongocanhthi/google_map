3ccca5369069b8e01e61e617a9cf1ef1f508a14f
Võ Ngọc Anh Thi <anhthi199x@gmail.com>
2021-07-12 11:33:17 +0700

    select map type

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,21 +3,71 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:flutter/cupertino.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _dropdownValue = "Normal";
+
+  List<String> _mapTypeTextList = [
+    "None",
+    "Normal",
+    "Satellite",
+    "Terrain",
+    "Hybrid"
+  ];
+
+  MapType _mapType = MapType.terrain;
+  List<MapType> _mapTypeList = [
+    MapType.none,
+    MapType.normal,
+    MapType.satellite,
+    MapType.terrain,
+    MapType.hybrid
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Map")),
+        title: Center(child: Text("Google Map")),
       ),
-      body: Container(
-        child: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(37.4219999, -122.0862462),
+      body: Column(
+        children: [
+          Row(children: [
+            Text("Select map type: "),
+            DropdownButton(
+              value: _dropdownValue,
+              onChanged: (String? value) {
+                setState(() {
+                  _dropdownValue = value!;
+                  for (int i = 0; i < _mapTypeTextList.length; i++) {
+                    if(_dropdownValue == _mapTypeTextList[i]){
+                      _mapType = _mapTypeList[i];
+                    }
+                  }
+                });
+              },
+              items: _mapTypeTextList.map<DropdownMenuItem<String>>((text) {
+                return DropdownMenuItem<String>(
+                  value: text,
+                  child: Text("$text"),
+                );
+              }).toList(),
+            )
+          ]),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(37.4219999, -122.0862462),
+              ),
+              onMapCreated: (controller) {},
+              mapType: _mapType,
+            ),
           ),
-          onMapCreated: (controller) {},
-          mapType: MapType.terrain,
-        ),
+        ],
       ),
     );
   }
